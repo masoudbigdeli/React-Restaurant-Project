6fa10ed37cfc221658cc57f2467a43133e105736
masoudbigdeli <masoud8bigdeli@gmail.com>
2022-08-30 17:26:33 +0430

    HOME-20220830

--- restaurant-app/src/components/SubHeading/SubHeading.jsx
+++ restaurant-app/src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+import { images } from '../../constants'
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginButton: 'rem'}}>
+    <p className='p__cormorant'>{title}</p>
+    <img src={images.spoon} alt="spoon" className='sponn__img'/>
   </div>
 );
 
--- restaurant-app/src/container/AboutUs/AboutUs.jsx
+++ restaurant-app/src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id="about">
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="letter G"/>
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+      </div>
+
+    </div>
   </div>
 );
 
