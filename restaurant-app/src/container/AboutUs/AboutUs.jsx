6231eb2d42c71f84def10e69380af40de35cab35
masoudbigdeli <masoud8bigdeli@gmail.com>
2022-08-31 02:42:10 +0430

    ISC-20220831

--- restaurant-app/src/container/AboutUs/AboutUs.jsx
+++ restaurant-app/src/container/AboutUs/AboutUs.jsx
@@ -12,6 +12,19 @@ const AboutUs = () => (
       <div className='app__aboutus-content_about'>
         <h1 className='headtext__cormorant'>About Us</h1>
         <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className="p__opensans">Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife"/>
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className="p__opensans">Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom Lorem Epsom</p>
+        <button type="button" className="custom__button">Know More</button>
       </div>
 
     </div>
